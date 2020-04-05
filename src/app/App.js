def5fa5498389cb8501f039c47ab1b453f8ff8f1
Kit Jonathan Te <kjonathante@gmail.com>
2020-04-05 15:28:25 -0700

    Analyzed mapStateToProps flow

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Items from '../features/items/Items'
 
-const App = () => <Items />
+const App = () => <Items someName={{ property: 'value' }} />
 
 export default App
--- src/features/items/Items.js
+++ src/features/items/Items.js
@@ -2,18 +2,19 @@ import React from 'react'
 import Item from './Item'
 import { connect } from 'react-redux'
 
-const mapStateToProps = (state) => {
-  console.log('mapStateToProps', state)
+const mapStateToProps = (state, ownProps) => {
+  console.log('mapStateToProps', state, ownProps)
   return {
     items: state.items,
   }
 }
 
 const Items = (props) => {
-  console.log(props)
+  console.log('Items component', props)
+  const { items } = props
   return (
     <div>
-      {props.items.map((e) => (
+      {items.map((e) => (
         <Item key={e.id}>{e.attribute}</Item>
       ))}
     </div>
